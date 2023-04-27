54f1300fcf10aa3bf28c7177025eb86e21cbeebc
Alexandr Zuev <zuevalex@yandex.ru>
2023-04-27 22:16:10 +0500

    123

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Main from "./Main";
 import PopupWithForm from "./PopupWithForm";
+//import PopupWithForm from "./PopupWithForm";
 import ImagePopup from "./ImagePopup";
 
 function App() {
